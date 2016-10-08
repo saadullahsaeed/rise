d460b0ac2b3bbf39839b086679e739a5c6bd3d02
Peter Jihoon Kim <raingrove@gmail.com>
2016-10-07 18:02:14 -0700

    middleware.Stack class

---
+++ src/middleware.js
@@ -0,0 +1,63 @@
+'use strict';
+
+class Stack {
+  constructor() {
+    this._stack = [];
+    this._currIndex = 0;
+  }
+
+  _defaultErrorHandler(err) {
+    if (err instanceof Error && typeof err.stack === 'string') {
+      process.stderr.write(err.stack + "\n");
+      return;
+    }
+    process.stderr.write(String(err) + "\n");
+  }
+
+  push(middleware) {
+    if (typeof middleware !== 'function') {
+      throw new TypeError("'middleware' must be a function");
+    }
+    this._stack.push(middleware);
+    return this;
+  }
+
+  run(req, res, err) {
+    const stack = this._stack,
+          fn = stack[this._currIndex];
+
+    if (!fn) {
+      return;
+    }
+
+    const nextFn = (err) => {
+      if (err != null) {
+        const stack = this._stack;
+        let fn;
+
+        do {
+          this._currIndex++;
+          fn = stack[this._currIndex];
+          if (!fn) {
+            this._defaultErrorHandler(err);
+            return;
+          }
+        } while (fn.length < 4);
+      } else {
+        this._currIndex++;
+      }
+
+      this.run(req, res, err);
+    };
+
+    if (fn.length < 4) {
+      fn(req, res, nextFn);
+    } else {
+      fn(err, req, res, nextFn);
+    }
+  }
+}
+
+module.exports = {
+  Stack
+};
