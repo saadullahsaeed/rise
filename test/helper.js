23e3bb8c09b2d9f3bf23fa7be0c96af9159cffc0
Peter Jihoon Kim <raingrove@gmail.com>
2016-10-10 12:10:53 -0700

    waitUntil helper for async tests

--- test/helper.js
+++ test/helper.js
@@ -8,3 +8,24 @@ root.expect = chai.expect;
 root.sinon = sinon;
 
 chai.use(sinonChai);
+
+root.waitUntil = function(conditionFunc, timeoutMs, intervalMs) {
+  timeoutMs = timeoutMs || 1000;
+  intervalMs = intervalMs || 1;
+
+  return new Promise(function(resolve, reject) {
+    const startTime = +new Date();
+
+    const timer = setInterval(function() {
+      if (conditionFunc()) {
+        clearInterval(timer);
+        resolve();
+        return;
+      }
+      if ((+new Date() - startTime) > timeoutMs) {
+        clearInterval(timer);
+        reject(new Error('waitUntil timed out'));
+      }
+    }, intervalMs);
+  });
+};
