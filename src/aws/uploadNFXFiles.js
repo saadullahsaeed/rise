be47a4eaee128aa29174bb58e14fe8d401502ec0
Sunjin Lee <styner32@gmail.com>
2016-10-14 19:42:55 -0400

    use promise from s3 sdk to upload nfx files

--- src/aws/uploadNFXFiles.js
+++ src/aws/uploadNFXFiles.js
@@ -1,65 +1,52 @@
 'use strict';
 
-const fs         = require('fs'),
-      path       = require('path'),
+const fs = require('fs'),
       log = require('../utils/log');
 
-module.exports.uploadNFXFiles = function(nfx) {
-  return new Promise((resolve, reject) => {
-    nfx.state = 'SAVING';
-    const uploadS3Promises = [];
+module.exports = function uploadNFXFiles(nfx) {
+  const s3 = nfx.aws.s3,
+        bucketName = nfx.bucketName,
+        uploadS3Promises = [];
 
-    const nfxFiles = [{
-      key: `versions/${nfx.version}/aws/cf.json`,
-      body: JSON.stringify(nfx.cfTemplate, null, 2),
-      contentType: 'application/json'
-    }, {
-      key: `versions/${nfx.version}/api.yaml`,
-      body: fs.createReadStream('api.yaml'),
-      contentType: 'text/yaml'
-    }, {
-      key: `versions/${nfx.version}/functions.yaml`,
-      body: fs.createReadStream('functions.yaml'),
-      contentType: 'text/yaml'
-    }, {
-      key: 'nfx.json',
-      body: JSON.stringify(nfx.nfxJSON),
-      contentType: 'application/json'
-    }];
+  const nfxFiles = [{
+    key: `versions/${nfx.version}/aws/cf.json`,
+    body: JSON.stringify(nfx.cfTemplate, null, 2),
+    contentType: 'application/json'
+  }, {
+    key: `versions/${nfx.version}/api.yaml`,
+    body: fs.createReadStream('api.yaml'),
+    contentType: 'text/yaml'
+  }, {
+    key: `versions/${nfx.version}/functions.yaml`,
+    body: fs.createReadStream('functions.yaml'),
+    contentType: 'text/yaml'
+  }, {
+    key: 'nfx.json',
+    body: JSON.stringify(nfx.nfxJSON),
+    contentType: 'application/json'
+  }];
 
-    for (let i = 0; i < nfxFiles.length; i++) {
-      uploadS3Promises.push(
-        uploadS3(nfx, nfxFiles[i].key, nfxFiles[i].body, nfxFiles[i].contentType)
-      );
-    }
+  for (let i = 0; i < nfxFiles.length; i++) {
+    uploadS3Promises.push(
+      upload(s3, bucketName, nfxFiles[i].key, nfxFiles[i].body, nfxFiles[i].contentType)
+    );
+  }
 
-    Promise.all(uploadS3Promises).then(() => {
-      resolve(nfx);
-    }, (err) => {
-      reject(err);
-    });
-  });
-}
+  return Promise.all(uploadS3Promises)
+        .then(function() {
+          return Promise.resolve(nfx);
+        });
+};
 
-function uploadS3(nfx, key, body, contentType) {
-  return new Promise((resolve, reject) => {
-    const params = {
-      Bucket: nfx.bucketName,
-      Key: key,
-      ACL: 'private',
-      Body: body,
-      ContentType: contentType
-    };
+function upload(s3, bucketName, key, body, contentType) {
+  const params = {
+    Bucket: bucketName,
+    Key: key,
+    ACL: 'private',
+    Body: body,
+    ContentType: contentType
+  };
 
-    log.info(`Saving ${key} in S3...`);
-    nfx.awsSDK.s3.upload(params, function(err, data) {
-      if (err) {
-        log.error(`Error on saving template ${err}`);
-        reject(err);
-      }
-
-      log.info(`Successfully saved ${key}`);
-      resolve();
-    });
-  });
+  log.info(`Saving ${key} in S3...`);
+  return s3.putObject(params).promise();
 }
