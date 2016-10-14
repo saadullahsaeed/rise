ba7687f644234eb003269cd91e8d44ed361603a8
Sunjin Lee <styner32@gmail.com>
2016-10-14 18:52:12 -0400

    use promise from s3 sdk to deploy API

--- src/aws/deployAPI.js
+++ src/aws/deployAPI.js
@@ -1,66 +1,66 @@
 'use strict';
 
-const fs         = require('fs'),
-      path       = require('path'),
+const path = require('path'),
       log = require('../utils/log'),
       fsReadFile = require('../utils/fs').fsReadFile;
 
-module.exports.deployAPI = function(nfx, options) {
-  return new Promise((resolve, reject) => {
-    nfx.state = 'DEPLOYING';
-    setDeployment(nfx, options.rollback);
+module.exports = function deployAPI(nfx, options) {
+  const cf = nfx.aws.cf;
 
-    const req = nfx.awsSDK.cf.updateStack({
-      StackName: nfx.stackName,
-      TemplateBody: JSON.stringify(nfx.cfTemplate, null, 2),
-      Capabilities: ['CAPABILITY_IAM']
-    });
+  nfx.cfTemplate.Resources = Object.assign({}, nfx.cfTemplate.Resources, getDeploymentResource(nfx.version, nfx.stage, options.rollback));
+  nfx.cfTemplate.Outputs = Object.assign({}, nfx.cfTemplate.Outputs, getDeploymentOutput(nfx.stage));
 
-    req.on('success', function(resp) {
-      log.info(`Updating stack with new deployment ${nfx.version}...`);
-      nfx.awsSDK.cf.waitFor('stackUpdateComplete',
-        { StackName: nfx.stackName },
-        function(err, data) {
-          if (err) {
-            reject(err);
-            return;
-          }
-
-          nfx.nfxJSON.active_version = nfx.version;
-
-          log.info('Successfully deployed.');
-          resolve(nfx);
-        }
-      );
-    });
-
-    req.on('error', function(err, data) {
+  return cf.updateStack({
+    StackName: nfx.stackName,
+    TemplateBody: JSON.stringify(nfx.cfTemplate, null, 2),
+    Capabilities: ['CAPABILITY_IAM']
+  }).promise()
+    .then(function() {
+      return waitForDeploy(nfx);
+    })
+    .catch((err) => {
       if (err.message && err.message.indexOf('No updates are to be performed') !== -1) {
         log.info("No updates on deployment. Proceed to the next step");
-        resolve(nfx);
+        return Promise.resolve(nfx);
       } else {
-        reject(err);
+        return Promise.reject(err);
       }
     });
+};
 
-    req.send();
-  });
+function waitForDeploy(nfx) {
+  const cf = nfx.aws.cf;
+
+  nfx.state = 'DEPLOYING';
+  log.info(`Updating stack [${nfx.stackName}] to deploy API...`);
+  return cf.waitFor('stackUpdateComplete', { StackName: nfx.stackName }).promise()
+    .then(() => {
+      log.info(`Updated stack [${nfx.stackName}] to deploy API...`);
+      nfx.nfxJSON.active_version = nfx.version;
+      nfx.state = 'DEPLOYED';
+      return Promise.resolve(nfx);
+    });
 }
 
-function setDeployment(nfx, rollback) {
-  let cfDeploymentContent = fsReadFile(path.join(__dirname, 'cf-deployment.json'));
+function getDeploymentResource(version, stage, rollback) {
+  const cfDeploymentContent = fsReadFile(path.join(__dirname, 'cf-deployment.json')),
+        result = {};
 
-  cfDeploymentContent = cfDeploymentContent.replace('$STAGE_NAME', nfx.stage);
-  const cfDeploymentJSON = JSON.parse(cfDeploymentContent);
-  let deploymentResourceName = `NFXDeployment${nfx.version}`;
+  let deploymentResourceName = `NFXDeployment${version}`;
   if (rollback) {
     // If we don't create new deployment tag with new name,
     // it will not deploy since there is no changes on deployment tag.
     deploymentResourceName = `${deploymentResourceName}Rollback`;
   }
-  nfx.cfTemplate.Resources[deploymentResourceName] = JSON.parse(cfDeploymentContent);
 
-  let cfBaseURLOutputContent = fsReadFile(path.join(__dirname, 'cf-api-base-url-output.json'));
-  cfBaseURLOutputContent = cfBaseURLOutputContent.replace('$STAGE_NAME', nfx.stage);
-  nfx.cfTemplate.Outputs.NFXBaseURL = JSON.parse(cfBaseURLOutputContent);
+  result[deploymentResourceName] = JSON.parse(cfDeploymentContent.replace('$STAGE_NAME', stage));
+  return result;
+}
+
+function getDeploymentOutput(stage) {
+  const cfBaseURLOutputContent = fsReadFile(path.join(__dirname, 'cf-api-base-url-output.json')),
+        result = {};
+
+  result.NFXBaseURL = JSON.parse(cfBaseURLOutputContent.replace('$STAGE_NAME', stage));
+  return result;
 }
