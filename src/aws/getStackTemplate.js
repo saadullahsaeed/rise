dde581dedf236789546c1cd3b25a4c8e61c55359
Sunjin Lee <styner32@gmail.com>
2016-10-14 21:04:28 -0400

    use promise from s3 sdk to fetch stack template

--- src/aws/getStackTemplate.js
+++ src/aws/getStackTemplate.js
@@ -2,18 +2,12 @@
 
 const log = require('../utils/log');
 
-module.exports.getStackTemplate = function(nfx) {
-  return new Promise((resolve, reject) => {
-    nfx.awsSDK.cf.getTemplate(
-      { StackName: nfx.stackName },
-      function(err, data) {
-        if (err) {
-          reject(err);
-        }
+module.exports = function getStackTemplate(nfx) {
+  return nfx.aws.cf.getTemplate(
+    { StackName: nfx.stackName }).promise()
+      .then(function(data) {
         log.info(`Successfully fetched the template`);
         nfx.cfTemplate = JSON.parse(data.TemplateBody);
-        resolve(nfx);
-      }
-    );
-  });
-}
+        return Promise.resolve(nfx);
+      });
+};
