800c2f12cb90fe78be35459ae3896498f6ca695a
Sunjin Lee <styner32@gmail.com>
2016-10-17 14:00:00 -0400

    refactor deploy/rollback.js

--- src/rollback/index.js
+++ src/rollback/index.js
@@ -1,31 +1,30 @@
 "use strict";
 
-const AWS                  = require('aws-sdk'),
-      getStack = require('../aws/getStack'),
+const getStack = require('../aws/getStack'),
       rollback = require('../aws/rollback'),
-      deployAPI            = require('../aws/deployAPI').deployAPI,
-      getStackTemplate     = require('../aws/getStackTemplate').getStackTemplate,
+      deployAPI = require('../aws/deployAPI'),
+      getStackTemplate = require('../aws/getStackTemplate'),
       fetchVersion = require('../aws/fetchVersion'),
-      handleInterrupt      = require('../aws/handleInterrupt').handleInterrupt,
-      uploadNFXFiles       = require('../aws/uploadNFXFiles').uploadNFXFiles,
+      uploadNFXFiles = require('../aws/uploadNFXFiles'),
+      handleInterrupt = require('../aws/handleInterrupt'),
       log = require('../utils/log');
 
 module.exports = function(nfx, version) {
   log.info('Rolling back to a previous version...');
 
-  // FIXME: It should be configurable.
-  nfx.stage = 'staging';
-  nfx.state = 'ROLLING_BACK';
-
   getStack(nfx)
-    .then(fetchVersion(nfx))
-    .then(rollback(nfx, version))
+    .then(fetchVersion)
+    .then(function(nfx) {
+      return rollback(nfx, version);
+    })
     .then(getStackTemplate(nfx))
-    .then(deployAPI(nfx, { rollback: true }))
-    .then(uploadNFXFiles(nfx))
+    .then(function(nfx) {
+      return deployAPI(nfx, { rollback: true });
+    })
+    .then(uploadNFXFiles)
     .catch(log.error);
 
-  process.on('SIGINT', function () {
+  process.on('SIGINT', function() {
     handleInterrupt(nfx);
   });
-}
+};
