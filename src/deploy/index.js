db869889fac7a99bfd93c5ea05c0f96cfae9a893
Sunjin Lee <styner32@gmail.com>
2016-10-17 13:59:45 -0400

    refactor deploy/index.js

--- src/deploy/index.js
+++ src/deploy/index.js
@@ -2,31 +2,29 @@
 
 const log = require('../utils/log'),
       compressAndCompare = require('../aws/compressAndCompare'),
-      uploadFunctions       = require('../aws/uploadFunctions').uploadFunctions,
+      uploadFunctions = require('../aws/uploadFunctions'),
       getStack = require('../aws/getStack'),
       getBucket = require('../aws/getBucket'),
       fetchVersion = require('../aws/fetchVersion'),
-      updateTemplate        = require('../aws/updateTemplate').updateTemplate,
-      deployAPI             = require('../aws/deployAPI').deployAPI,
-      uploadNFXFiles        = require('../aws/uploadNFXFiles').uploadNFXFiles,
-      handleInterrupt       = require('../aws/handleInterrupt').handleInterrupt;
+      updateStack = require('../aws/updateStack'),
+      deployAPI = require('../aws/deployAPI'),
+      uploadNFXFiles = require('../aws/uploadNFXFiles'),
+      handleInterrupt = require('../aws/handleInterrupt');
 
-module.exports = (nfx) => {
-  // FIXME: It should be configurable.
-  nfx.stage = 'staging';
-  nfx.state = 'CREATING';
-
+module.exports = function(nfx) {
   getBucket(nfx)
-    .then(getStack(nfx))
-    .then(fetchVersion(nfx))
-    .then(compressAndCompare(nfx))
-    .then(uploadFunctions(nfx))
-    .then(updateTemplate(nfx)) // Takes at least 30 secs
-    .then(deployAPI(nfx, {})) // Takes at least 30 secs
-    .then(uploadNFXFiles(nfx))
+    .then(getStack)
+    .then(fetchVersion)
+    .then(compressAndCompare)
+    .then(uploadFunctions)
+    .then(updateStack)
+    .then(function(nfx) {
+      return deployAPI(nfx, {});
+    })
+    .then(uploadNFXFiles)
     .catch(log.error);
 
-  process.on('SIGINT', function () {
+  process.on('SIGINT', function() {
     handleInterrupt(nfx);
   });
 };
