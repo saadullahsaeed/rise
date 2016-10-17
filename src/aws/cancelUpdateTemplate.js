7557ee987060e308b5bee99589c5b60454a0e06d
Sunjin Lee <styner32@gmail.com>
2016-10-17 12:57:53 -0400

    use promise from s3 sdk to cancel deploying

--- src/aws/cancelUpdateTemplate.js
+++ src/aws/cancelUpdateTemplate.js
@@ -1,41 +1,39 @@
 'use strict';
 
-const fs         = require('fs'),
-      path       = require('path'),
-      log = require('../utils/log'),
-      fsReadFile = require('../utils/fs').fsReadFile;
+const log = require('../utils/log');
 
-module.exports.cancelUpdateTemplate = function(nfx) {
+module.exports = function cancelUpdateTemplate(nfx) {
   return new Promise((resolve, reject) => {
     const params = { StackName: nfx.stackName },
-          cf = nfx.awsSDK.cf,
+          cf = nfx.aws.cf,
+          checkStackState = function(err, data) {
+            if (err) {
+              reject(err);
+              return;
+            }
+
+            const status = data.Stacks[0].StackStatus;
+            if (status !== 'UPDATE_ROLLBACK_COMPLETE') {
+              setTimeout(function() {
+                cf.describeStacks(params, checkStackState);
+              }, 5000);
+            } else {
+              nfx.state = 'CANCELLED';
+              resolve(nfx);
+            }
+          },
           req = cf.cancelUpdateStack(params);
 
-    const checkStackState = function(err, data) {
-      if (err) {
-        reject(err);
-        return;
-      }
-
-      var status = data.Stacks[0].StackStatus
-      if (status !== 'UPDATE_ROLLBACK_COMPLETE') {
-        setTimeout(function() {
-          cf.describeStacks(params, checkStackState);
-        }, 5000);
-      } else {
-        resolve(nfx);
-      }
-    };
-
-    log.info('Canceling Updating stack...');
-    req.on('success', function(resp) {
+    nfx.state = 'CANCELLING';
+    req.on('success', function(/* resp */) {
+      log.info('Cancelling Updating stack...');
       cf.describeStacks(params, checkStackState);
     });
 
-    req.on('error', function(err, data) {
+    req.on('error', function(err/*,  data */) {
       // This is when the update is done before the request was made
       if (err.message && err.message.indexOf('CancelUpdateStack cannot be called from current stack status') !== -1) {
-        log.info('The stack already has updated.');
+        log.info('The stack already has been updated.');
         nfx.state = 'UNEXPECTEDLY_UPDATED';
         resolve(nfx);
       } else {
@@ -45,5 +43,4 @@ module.exports.cancelUpdateTemplate = function(nfx) {
 
     req.send();
   });
-}
-
+};
--- src/aws/handleInterrupt.js
+++ src/aws/handleInterrupt.js
@@ -1,39 +1,39 @@
 'use strict';
 
-const fs                   = require('fs'),
-      log = require('../utils/log'),
-      cancelUpdateTemplate = require('../aws/cancelUpdateTemplate').cancelUpdateTemplate,
+const log = require('../utils/log'),
+      cancelUpdateTemplate = require('../aws/cancelUpdateTemplate'),
       rollback = require('../aws/rollback');
 
-module.exports.handleInterrupt = function(nfx) {
-  console.log(`SIGINT fired at ${nfx.state}`);
-  if (nfx.state === 'UPDATING') {
+module.exports = function handleInterrupt(nfx) {
+  log.info(`SIGINT fired at ${nfx.state}`);
+  if (nfx.state === 'CREATING') {
+    log.info('Creating stack is still in progress. Please run `nfx destroy`.');
+    process.exit(1);
+  } else if (nfx.state === 'UPDATING') {
     // Users could send Ctrl+c again.
     nfx.state = 'REVERTING';
-    console.log('Canceling updating stack');
     cancelUpdateTemplate(nfx)
       .then(function(nfx) {
-        if (nfx.state == 'UNEXPECTEDLY_UPDATED') {
+        if (nfx.state === 'UNEXPECTEDLY_UPDATED') {
           // When the stack is updated before cancelling,
           // we update the stack to previous version
           // Nothing to rollback if this is the first deployment.
           if (nfx.version !== 'v1') {
             const activeVersion = nfx.nfxJSON.active_version;
-            log.info(`the deployment have been cancelled. Rolling back to "${activeVersion}"`);
-            rollback(nfx, activeVersion).then( () => {
-              console.log('Successfully rolled back.');
+            log.info(`The deployment have been cancelled. Rolling back to "${activeVersion}"`);
+            rollback(nfx, activeVersion).then(() => {
+              log.info('Successfully rolled back.');
               process.exit(1);
             }).catch(function(err) {
-              if (err.stack) {
-                log.error(err.stack);
-              } else {
-                log.error(err);
-              }
+              log.error(err);
               process.exit(1);
             });
+          } else {
+            log.info('Updating the stack has been completed. The deployment has been cancelled');
+            process.exit(1);
           }
         } else {
-          log.info('cancelled');
+          log.info('Updating the stack has been cancelled.');
           process.exit(1);
         }
       })
@@ -46,8 +46,6 @@ module.exports.handleInterrupt = function(nfx) {
         process.exit(1);
       });
   } else if (nfx.state === 'DEPLOYING') {
-    nfx.state = 'REVERTING';
-    console.log('Canceling deploying');
     cancelUpdateTemplate(nfx)
       .then(function(nfx) {
         // Nothing to rollback if this is the first deployment.
@@ -58,21 +56,17 @@ module.exports.handleInterrupt = function(nfx) {
         }
       })
       .then(function() {
-        console.log('Successfully rolled back.');
+        log.info('Successfully rolled back');
         process.exit(1);
       })
       .catch(function(err) {
-        if (err.stack) {
-          log.error(err.stack);
-        } else {
-          log.error(err);
-        }
+        log.error(err);
         process.exit(1);
       });
-  } else if (nfx.state === 'REVERTING' || nfx.state === 'UNEXPECTEDLY_UPDATED') {
-    console.log('Reverting is in progress ... Please wait until reverting is done');
+  } else if (nfx.state === 'CANCELLING' || nfx.state === 'UNEXPECTEDLY_UPDATED') {
+    log.info('Reverting is in progress ... Please wait until reverting is done');
   } else {
     process.exit(1);
   }
-}
+};
 
