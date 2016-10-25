13d2747b1618792901d7c4285738799b8eca512b
Sunjin Lee <styner32@gmail.com>
2016-10-24 18:40:27 -0700

    remove destroy command

--- src/destroy/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-const log = require('../utils/log'),
-      deleteStack = require('../aws/deleteStack');
-
-module.exports = function(nfx) {
-  deleteStack(nfx).catch(log.error);
-};
