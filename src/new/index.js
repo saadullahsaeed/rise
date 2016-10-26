29b7a7fb2a981f9166cad959012ba11e4e26fc5e
Sunjin Lee <styner32@gmail.com>
2016-10-26 15:13:25 -0700

    fix regions based on http://docs.aws.amazon.com/general/latest/gr/rande.html

--- src/new/index.js
+++ src/new/index.js
@@ -9,7 +9,7 @@ const fs = require('fs'),
       fsStat = require('../utils/fs').fsStat;
 
 const DEFAULT_PROVIDER = 'aws',
-      REGIONS = ['us-east-1', 'us-west-1', 'eu-west-1', 'ap-southeast-1', 'Other'];
+      REGIONS = ['us-east-1', 'us-east-2', 'us-west-2', 'eu-central-1', 'eu-west-1', 'ap-southeast-1', 'ap-southeast-2', 'ap-northeast-1', 'ap-northeast-2'];
 
 module.exports = function(stackName, options) {
   let region = options.region,
